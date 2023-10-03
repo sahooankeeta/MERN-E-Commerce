20703d7126b20eff936bc7827b5108815cb2af72
Ankeeta Sahoo <sahooankeeta@gmail.com>
2023-10-03 13:32:51 +0530

    multer

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -3,9 +3,9 @@ const path = require("path");
 // Multer config
 module.exports = multer({
   storage: multer.diskStorage({
-    destination:(req,res,cb)=>{
-        cb(null,path.join(__dirname,'../public'))
-    },
+    // destination:(req,res,cb)=>{
+    //     cb(null,path.join(__dirname,'../public'))
+    // },
     filename:(req,file,cb)=>{
         cb(null,file.fieldname+"_"+Date.now()+path.extname(file.originalname))
     }
